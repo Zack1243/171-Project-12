c4389b7a92b9af7cc32dd1ebdb744473d7d7ddb1
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2023-11-27 14:36:50 -0800

    skeleton web

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    r
