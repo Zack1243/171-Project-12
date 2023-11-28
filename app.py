de482dce0414070fc92db377d39be2c6c0e483a1
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2023-11-27 17:26:51 -0800

    model predict

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
 from flask import Flask, request, render_template
+import pickle
 
 app = Flask(__name__)
 
+model = pickle.load(open("model.pkl", "rb"))
+
+
 @app.route("/")
 def home():
-    return render_template('index.html')
+    return render_template("index.html")
 
-@app.route('/predict',methods=['POST'])
+
+@app.route("/predict", methods=["POST"])
 def predict():
-    return request.form.get("workclass")
+    prediction = model.predict(
+        [
+            [
+                int(request.form.get("age")),
+                request.form.get("workclass"),
+                int(request.form.get("fnlwgt")),
+                request.form.get("education"),
+                int(request.form.get("educational-num")),
+                request.form.get("marital-status"),
+                request.form.get("occupation"),
+                request.form.get("relationship"),
+                request.form.get("race"),
+                request.form.get("gender"),
+                int(request.form.get("capital-gain")),
+                int(request.form.get("capital-loss")),
+                int(request.form.get("hours-per-week"))
+            ]
+        ]
+    )
+    return prediction
