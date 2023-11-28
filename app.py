f449c17f99aeb77fe67319f7488a6dec423054ca
Ryan-Vakhshoori <96512123+Ryan-Vakhshoori@users.noreply.github.com>
2023-11-28 10:43:11 -0800

    frontend prediction

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
+# flask --app app run
+
 from flask import Flask, request, render_template
 import pickle
+import pandas as pd
 
 app = Flask(__name__)
 
 model = pickle.load(open("model.pkl", "rb"))
-
+ohe = pickle.load(open("ohe.pkl", "rb"))
+scaler = pickle.load(open("scaler.pkl", "rb"))
 
 @app.route("/")
 def home():
@@ -13,23 +17,28 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    prediction = model.predict(
-        [
-            [
-                int(request.form.get("age")),
-                request.form.get("workclass"),
-                int(request.form.get("fnlwgt")),
-                request.form.get("education"),
-                int(request.form.get("educational-num")),
-                request.form.get("marital-status"),
-                request.form.get("occupation"),
-                request.form.get("relationship"),
-                request.form.get("race"),
-                request.form.get("gender"),
-                int(request.form.get("capital-gain")),
-                int(request.form.get("capital-loss")),
-                int(request.form.get("hours-per-week"))
-            ]
-        ]
-    )
-    return prediction
+    d = {
+        "age": [int(request.form.get("age"))],
+        "workclass": [request.form.get("workclass")],
+        "fnlwgt": [int(request.form.get("fnlwgt"))],
+        "education": [request.form.get("education")],
+        "educational-num": [int(request.form.get("educational-num"))],
+        "marital-status": [request.form.get("marital-status")],
+        "occupation": [request.form.get("occupation")],
+        "relationship": [request.form.get("relationship")],
+        "race": [request.form.get("race")],
+        "gender": [request.form.get("gender")],
+        "capital-gain": [int(request.form.get("capital-gain"))],
+        "capital-loss": [int(request.form.get("capital-loss"))],
+        "hours-per-week": [int(request.form.get("hours-per-week"))],
+    }
+    df = pd.DataFrame(data=d, index=[0])
+    columns_to_encode = ['education', 'workclass', 'marital-status', 'occupation', 'relationship', 'race', 'gender']
+    encoded_columns = ohe.transform(df[columns_to_encode])
+    encoded_df = pd.DataFrame(encoded_columns, columns=ohe.get_feature_names_out(columns_to_encode))
+    df_encoded = pd.concat([df.drop(columns=columns_to_encode), encoded_df], axis=1)
+    scaled_input = scaler.transform(df_encoded)
+    pred = model.predict(scaled_input)
+    if pred == [1]:
+        return '>50k'
+    return '<50k'
